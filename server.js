5947264a94afa84ca09659e6c5e3a238291d9588
Chheany Mok <cmok4290@gmail.com>
2020-05-20 17:47:43 -0400

    Add https server options

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 // server.js
 'use strict';
 const app = require('express')();
+const cors = require('cors');
+const fs = require('fs');
 const http = require('http').createServer(app);
+const options = {
+  cert: fs.readFileSync("./cert.pem"),
+  key: fs.readFileSync("./key.pem"),
+  ca: fs.readFileSync("./ca.pem")
+};
+const https = require('https').createServer(options, app);
 const SocketWrapper = require('./SocketWrapper');
 
 // constants
-const PORT = 8080;
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
 const HOST = '127.0.0.1';
+
+app.use(cors());
 
 // app
 app.get('/', (req, res) => {
   res.send('Terminal Server Running...');
 });
 
-http.listen(PORT, () => {
+http.listen(HTTP_PORT, () => {
+  //const socket = new SocketWrapper();
+  //socket.attachServer(http);
+  console.log(`Running on http://${HOST}:${HTTP_PORT}`);
+});
+
+https.listen(HTTPS_PORT, () => {
   const socket = new SocketWrapper();
-  socket.attachServer(http);
-  console.log(`Running on http://${HOST}:${PORT}`);
+  socket.attachServer(https);
+  console.log(`Running on https://${HOST}:${HTTP_PORT}`);
 });
