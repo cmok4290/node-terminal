a465b4e1629f3a315efe0c877692aef1d87bd6a7
Chheany Mok <cmok4290@gmail.com>
2020-07-02 12:54:12 -0400

    Update cors options

--- server.js
+++ server.js
@@ -17,14 +17,7 @@ let origins = [
 const app = express();
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (origins.indexOf(origin) === -1) {
-        const message =
-          "The CORS policy for this origin doesn't allow access from that particular origin.";
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
+    origin: origins,
   })
 );
 
